ed9613626e414677fcf1874b3298b7a49c980959
PennyTrain <penny.train04@gmail.com>
2024-10-20 15:29:41 +0100

    Install boostrap, enhance menu-items and types

--- dynamic-menu/src/app/types.ts
+++ dynamic-menu/src/app/types.ts
@@ -1,12 +1,7 @@
 export const types = [
-    { id: 1, name: 'Group 1' },
-    { id: 2, name: 'Group 2' },
-    { id: 3, name: 'Group 3' },
-    { id: 4, name: 'Group 4' },
-    { id: 5, name: 'Group 5' },
-    { id: 6, name: 'Group 6' },
-    { id: 7, name: 'Group 7' },
-    { id: 8, name: 'Group 8' },
-    { id: 9, name: 'Group 9' }
-  ];
-  
+  { id: 1, name: 'Hardware' },
+  { id: 2, name: 'Conditioning' },
+  { id: 3, name: 'Main' },
+  { id: 4, name: 'CJC' },
+  { id: 5, name: 'UserCalibration' }
+];
